5d21d46dbc227c29af8b8363bef39aa7dc829ab3
Chocobo1 <Chocobo1@users.noreply.github.com>
2017-01-04 15:39:33 +0800

    Update Google+1 button

--- index.php
+++ index.php
@@ -13,14 +13,8 @@
 <iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.qbittorrent.org&amp;layout=standard&amp;show_faces=false&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:560px; height:35px;" allowTransparency="true"></iframe>
 <br/>
 <!-- Google + -->
-<g:plusone annotation="inline"></g:plusone>
-<script type="text/javascript">
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-</script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<div class="g-plusone" data-annotation="inline"></div>
 <br/><br/>
 <!-- Twitter -->
 <iframe allowtransparency="true" frameborder="0" scrolling="no"
