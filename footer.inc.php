0b12c11d64ccca90588177e4b31100f068aa0680
Chocobo1 <Chocobo1@users.noreply.github.com>
2017-01-29 21:14:10 +0800

    Clean up footer

--- footer.inc.php
+++ footer.inc.php
@@ -3,11 +3,14 @@
           </div>
         </div>
       </div>
-      <p class="copyright">Bitcoin: <a href=bitcoin:1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU>1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU</a><br/></p>
-      <p class="copyright">Litecoin: <a href=litecoin:LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH>LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH</a><br/><br/></p>
-      <p class="copyright">Copyright &copy;2006-2016 <a href="mailto:sledgehammer999@qbittorrent.org">The qBittorrent project</a> - All rights reserved<br/><br/>
+      <p class="copyright">
+        Bitcoin: <a href=bitcoin:1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU style="color: #FFFFFF;">1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU</a><br/>
+        Litecoin: <a href=litecoin:LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH style="color: #FFFFFF;">LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH</a><br/>
+        <br/>
+        Copyright &copy;2006-2017 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
+        <br/>
         <a href="donate"><img src="https://sourceforge.net/images/project-support.jpg" width="88" height="32" alt="Support this project" /></a>
-        <a href="https://sourceforge.net/projects/qbittorrent"><img src="https://sourceforge.net/sflogo.php?group_id=163414&type=1" width="88" height="31" alt="SourceForge.net logo" /></a><br/>
+        <a href="https://sourceforge.net/projects/qbittorrent"><img src="https://sourceforge.net/sflogo.php?group_id=163414&type=1" width="88" height="31" alt="SourceForge.net logo" /></a>
       </p>
     </div>
   </body>
