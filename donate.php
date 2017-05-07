9b2f238fa8e5ee69e3c84f411eefc5f510a70d24
sledgehammer_999 <hammered999@gmail.com>
2017-05-07 04:42:42 +0300

    Temporarily disable paypal donation option.

--- donate.php
+++ donate.php
@@ -5,8 +5,7 @@
 <p>Bitcoin donations are preferred.</p>
 <ul>
   <li><a href="bitcoin:1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU">Bitcoin</a></li>
-  <li><a href="litecoin:LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH">Litecoin</a></li>
-  <li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=VH4G363266398&lc=HU&item_name=qBittorrent%20donation&no_note=0&cn=Thanks%21%20Any%20message%20for%20our%20team%3f&no_shipping=1&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted">PayPal</a></li>
+  <li><a href="litecoin:LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH">Litecoin</a></li>  
   <li><a href="https://flattr.com/thing/3435969/qbittorrent">Flattr</a></li>
 </ul>
 
