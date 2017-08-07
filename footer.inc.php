3d3912e693172dc3db0883e56d0a74a073c2a6ce
Chocobo1 <Chocobo1@users.noreply.github.com>
2017-08-08 01:21:36 +0800

    Add quotes

--- footer.inc.php
+++ footer.inc.php
@@ -6,8 +6,8 @@
       <p class="copyright">
         Help us improve the <a href="https://github.com/qbittorrent/qBittorrent-website" style="color: #FFFFFF;">website</a><br/>
         <br/>
-        Bitcoin: <a href=bitcoin:1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU style="color: #FFFFFF;">1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU</a><br/>
-        Litecoin: <a href=litecoin:LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH style="color: #FFFFFF;">LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH</a><br/>
+        Bitcoin: <a href="bitcoin:1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU" style="color: #FFFFFF;">1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU</a><br/>
+        Litecoin: <a href="litecoin:LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH" style="color: #FFFFFF;">LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH</a><br/>
         <br/>
         Copyright &copy;2006-2017 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
         <br/>
