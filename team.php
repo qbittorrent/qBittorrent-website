5885ca46816b22cc4f5f2ef90e0ced98e5082ecb
Chocobo1 <Chocobo1@users.noreply.github.com>
2017-11-10 15:08:21 +0800

    Fix page to pass w3c validator

--- team.php
+++ team.php
@@ -3,65 +3,65 @@
 <h2>Team Members</h2>
 <h3>Project Maintainer</h3>
 <fieldset style="background-color: #efefef">
-<span class="myface" style="width: 96px; height: 96px; float: left;">
+<div class="myface" style="width: 96px; height: 96px; float: left;">
 <img src="img/team/unknown.png" alt="sledgehammer999"/>
-</span>
-<span class="info" style="height: 96px; float:left;">
+</div>
+<div class="info" style="height: 96px; float:left;">
 <ul>
 <li><b>Nickname:</b> sledgehammer999</li>
 <li><b>Role:</b> Project maintainer and main developer</li>
 <li><b>Country:</b> <img src="img/flags/greece.png" alt="Greek" class="flag"/></li>
 <li><b>Mail:</b> sledgehammer999 (at) qbittorrent (dot) org</li>
 </ul>
-</span>
+</div>
 </fieldset>
 <h3>Active Contributors</h3>
 <fieldset style="background-color: #efefef">
-<span class="myface" style="width: 96px; height: 96px; float: left;">
+<div class="myface" style="width: 96px; height: 96px; float: left;">
 <img src="img/team/unknown.png" alt="Nick Tiskov"/>
-</span>
-<span class="info" style="height: 96px; float:left;">
+</div>
+<div class="info" style="height: 96px; float:left;">
 <ul>
 <li><b>Name:</b> Nick Tiskov</li>
 <li><b>Role:</b> Development, Unofficial Windows 64bit builds</li>
 <li><b>Country:</b> <img src="img/flags/russia.png" alt="Russian" class="flag"/></li>
 <li><b>Mail:</b> daymansmail (at) gmail (dot) com</li>
 </ul>
-</span>
+</div>
 </fieldset>
 <fieldset style="background-color: #efefef">
-<span class="myface" style="width: 96px; height: 96px; float: left;">
+<div class="myface" style="width: 96px; height: 96px; float: left;">
 <img src="img/team/unknown.png" alt="Zsolt Peter Basak"/>
-</span>
-<span class="info" style="height: 96px; float:left;">
+</div>
+<div class="info" style="height: 96px; float:left;">
 <ul>
 <li><b>Name:</b> Zsolt Peter Basak</li>
 <li><b>Role:</b> Forum administrator and owner</li>
 <li><b>Country:</b> <img src="img/flags/hungary.png" alt="Hungarian" class="flag"/></li>
 <li><b>Mail:</b> zsoltpeterbasak (at) gmail (dot) com</li>
 </ul>
-</span>
+</div>
 </fieldset>
 <h3>Original Author (Inactive)</h3>
 <fieldset style="background-color: #efefef">
-<span class="myface" style="width: 96px; height: 116px; float: left;">
+<div class="myface" style="width: 96px; height: 116px; float: left;">
 <img src="img/team/chris.png" alt="Christophe Dumez ('chris')"/>
-</span>
-<span class="info" style="height: 118px; float:left;">
+</div>
+<div class="info" style="height: 118px; float:left;">
 <ul>
 <li><b>Name:</b> Christophe Dumez</li>
 <li><b>Role:</b> Original author (no longer active)</li>
 <li><b>Country:</b> <img src="img/flags/france.png" alt="French" class="flag"/></li>
 <li><b>Mail:</b> chris (at) qbittorrent (dot) org</li>
 </ul>
-</span>
+</div>
 </fieldset>
 <h3>Inactive Contributors</h3>
 <fieldset style="background-color: #efefef">
-<span class="myface" style="width: 96px; height: 96px; float: left;">
+<div class="myface" style="width: 96px; height: 96px; float: left;">
 <img src="img/team/ishan.jpg" alt="Ishan Arora"/>
-</span>
-<span class="info" style="height: 96px; float:left;">
+</div>
+<div class="info" style="height: 96px; float:left;">
 <ul>
 <li><b>Name:</b> Ishan Arora</li>
 <li><b>Role:</b> Development</li>
@@ -69,13 +69,13 @@
 <li><b>Mail:</b> ishan (at) qbittorrent (dot) org</li>
 <li><b>Homepage:</b> <a href="http://www.ishanarora.com/">here</a></li>
 </ul>
-</span>
+</div>
 </fieldset>
 <fieldset style="background-color: #efefef">
-<span class="myface" style="width: 96px; height: 96px; float: left;">
+<div class="myface" style="width: 96px; height: 96px; float: left;">
 <img src="img/team/santaris.jpg" alt="Stefanos Antaris"/>
-</span>
-<span class="info" style="height: 96px; float:left;">
+</div>
+<div class="info" style="height: 96px; float:left;">
 <ul>
 <li><b>Name:</b> Stefanos Antaris (aka "Santaris")</li>
 <li><b>Role:</b> Development, Mac OS maintainer</li>
@@ -83,13 +83,13 @@
 <li><b>Mail:</b> santaris (at) qbittorrent (dot) org</li>
 <li><b>LinkedIn:</b> <a href="http://www.linkedin.com/in/stefanosantaris">here</a></li>
 </ul>
-</span>
+</div>
 </fieldset>
 <fieldset style="background-color: #efefef">
-<span class="myface" style="width: 96px; height: 96px; float: left;">
+<div class="myface" style="width: 96px; height: 96px; float: left;">
 <img src="img/team/mdib.jpg" alt="Mohammad Dib"/>
-</span>
-<span class="info" style="height: 96px; float:left;">
+</div>
+<div class="info" style="height: 96px; float:left;">
 <ul>
 <li><b>Name:</b> Mohammad Dib</li>
 <li><b>Role:</b> Windows maintainer</li>
@@ -97,21 +97,21 @@
 <li><b>Mail:</b> mdib (at) qbittorrent (dot) org</li>
 <li><b>Homepage:</b> <a href="http://mkdib.free.fr">here</a></li>
 </ul>
-</span>
+</div>
 </fieldset>
 <h3>Project Graphist</h3>
 <fieldset style="background-color: #efefef">
-<span class="myface" style="width: 96px; height: 96px; float: left;">
+<div class="myface" style="width: 96px; height: 96px; float: left;">
 <img src="img/team/unknown.png" alt="Mateusz Tobola"/>
-</span>
-<span class="info" style="height: 96px; float:left;">
+</div>
+<div class="info" style="height: 96px; float:left;">
 <ul>
 <li><b>Name:</b> Mateusz Toboła (aka "Tobejodok")</li>
 <li><b>Role:</b> Software and Website artwork</li>
 <li><b>Country:</b> <img src="img/flags/poland.png" alt="Polish" class="flag"/></li>
 <li><b>Mail:</b> tobejodok (at) qbittorrent (dot) org</li>
 </ul>
-</span>
+</div>
 </fieldset>
 
 <h3>Translators</h3>
