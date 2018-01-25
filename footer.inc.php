08fe8d1f2abff3a6e3f021a805ea61a0e7b97668
Chocobo1 <Chocobo1@users.noreply.github.com>
2018-01-25 21:35:14 +0800

    Update footer img

--- footer.inc.php
+++ footer.inc.php
@@ -11,8 +11,8 @@
         <br/>
         Copyright &copy;2006-2018 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
         <br/>
-        <a href="donate"><img src="https://sourceforge.net/images/project-support.jpg" width="88" height="32" alt="Support this project"/></a>
-        <a href="https://sourceforge.net/projects/qbittorrent"><img src="https://sourceforge.net/sflogo.php?group_id=163414&type=1" width="88" height="31" alt="SourceForge.net logo"/></a>
+        <a href="donate"><img src="https://sourceforge.net/images/project-support.jpg" height="32" alt="Support this project"/></a>
+        <a href="https://sourceforge.net/projects/qbittorrent"><img src="https://sourceforge.net/sflogo.php?type=14&group_id=274282" height="32" alt="SourceForge.net logo"/></a>
       </p>
     </div>
   </body>
