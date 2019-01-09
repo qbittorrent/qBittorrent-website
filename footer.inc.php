6517513edd8ea1ea538949f3668f4f511b2073b1
Andrei Stepanov <adem4ik@gmail.com>
2019-01-09 13:18:03 +0400

    Update copyright year

--- footer.inc.php
+++ footer.inc.php
@@ -9,7 +9,7 @@
         Bitcoin: <a href="bitcoin:1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU" style="color: #FFFFFF;">1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU</a><br/>
         Litecoin: <a href="litecoin:LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH" style="color: #FFFFFF;">LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH</a><br/>
         <br/>
-        Copyright &copy;2006-2018 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
+        Copyright &copy;2006-2019 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
         <br/>
         <a href="donate"><img src="https://sourceforge.net/images/project-support.jpg" height="32" alt="Support this project"/></a>
         <a href="https://sourceforge.net/projects/qbittorrent"><img src="https://sourceforge.net/sflogo.php?type=14&group_id=163414" height="32" alt="SourceForge.net logo"/></a>
