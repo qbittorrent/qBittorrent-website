a9edd33a2f5388015c3aff3d60c4b630c4e9ffd4
Chocobo1 <Chocobo1@users.noreply.github.com>
2020-01-02 00:07:59 +0800

    Bump copyright year

--- footer.inc.php
+++ footer.inc.php
@@ -9,7 +9,7 @@
         Bitcoin: <a href="bitcoin:1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU" style="color: #FFFFFF;">1PuTGwQvCF5hjp1uMKy7PPJqbM8uTjRtvU</a><br/>
         Litecoin: <a href="litecoin:LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH" style="color: #FFFFFF;">LL8FeesJ7ijjLZGJn85wsKaMM63oomTAXH</a><br/>
         <br/>
-        Copyright &copy;2006-2019 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
+        Copyright &copy;2006-2020 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
         <br/>
       </p>
     </div>
