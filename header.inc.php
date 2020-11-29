e9bb87b528097edc6092b6f6df2bc810f5a09b5f
Poopooracoocoo <51363864+Poopooracoocoo@users.noreply.github.com>
2020-11-29 22:53:04 +1100

    Added viewport meta tag

--- header.inc.php
+++ header.inc.php
@@ -12,6 +12,7 @@
     <meta name="author" content="qBittorrent Development Team: https://github.com/qbittorrent">
     <meta name="robots" content="NOODP">
     <meta name="google-site-verification" content="2P14de1zV_NTk03Ebnc0wpPKWaeJoj7B9Tc_HtRkOfE">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>qBittorrent Official Website</title>
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" sizes="16x16 32x32 64x64">
