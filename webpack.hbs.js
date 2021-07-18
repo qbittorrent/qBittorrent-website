5415fccd5af68b807d2beef39756064ce6abf06e
Kaustubh Badrike <kaustubh.badrike15@siesgst.ac.in>
2021-07-18 12:06:40 +0530

    Renamed webpack handlebar specific config
    (cherry picked from commit 125e5b98ee4aeede57f416ac092b3c9c2039a2df)

--- webpack.hbs.js
+++ webpack.hbs.js
@@ -7,16 +7,16 @@ const webpackConfig = {
 
         new HandlebarsPlugin({
             // path to hbs entry file(s). Also supports nested directories if write path.join(process.cwd(), "app", "src", "**", "*.hbs"),
-            entry: path.join(process.cwd(), "*.hbs"),
+            entry: path.join(process.cwd(), "handlebars", "*.hbs"),
             // output path and filename(s). This should lie within the webpacks output-folder
             // if ommited, the input filepath stripped of its extension will be used
-            output: path.join(process.cwd(), "[name].html"),
+            output: path.join(process.cwd(), "html", "[name].html"),
             // you can also add a [path] variable, which will emit the files with their relative path, like
             // output: path.join(process.cwd(), "build", [path], "[name].html"),
 
             // globbed path to partials, where folder/filename is unique
             partials: [
-                path.join(process.cwd(), "partials", "*.hbs")
+                path.join(process.cwd(), "handlebars", "partials", "**", "*.hbs")
             ],
         })
     ]
