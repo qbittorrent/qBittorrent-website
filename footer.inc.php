5fb48b39ea0370811b267a19d36d60207c4580c2
MandipJoshi <90699866+MandipJoshi@users.noreply.github.com>
2022-01-19 09:20:04 +0545

    Update copyright date
    PR #176.

--- footer.inc.php
+++ footer.inc.php
@@ -6,7 +6,7 @@
       <p class="copyright">
         Help us improve the <a href="https://github.com/qbittorrent/qBittorrent-website" style="color: #FFFFFF;">website</a><br/>
         <br/>
-        Copyright &copy;2006-2021 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
+        Copyright &copy;2006-2022 <a href="mailto:sledgehammer999@qbittorrent.org" style="color: #FFFFFF;">The qBittorrent project</a> - All rights reserved<br/>
         <br/>
       </p>
     </div>
