258fbde9ef10414308b662a42ce3826c03fd26e4
Chocobo1 <Chocobo1@users.noreply.github.com>
2025-01-21 20:35:13 +0800

    Migrate stylelint configuration

---
+++ stylelint.config.mjs
@@ -0,0 +1,18 @@
+export default {
+    extends: "stylelint-config-standard",
+    plugins: [
+        "stylelint-order"
+    ],
+    rules: {
+        "color-hex-length": null,
+        "comment-empty-line-before": null,
+        "comment-whitespace-inside": null,
+        "function-name-case": null,
+        "length-zero-no-unit": null,
+        "no-descending-specificity": null,
+        "order/properties-alphabetical-order": true,
+        "selector-class-pattern": null,
+        "selector-id-pattern": null,
+        "value-keyword-case": null
+    }
+};
