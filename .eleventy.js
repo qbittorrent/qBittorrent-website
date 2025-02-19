19d03221dfb16a9b332f4c64b2b4af737d95a0c9
Chocobo1 <Chocobo1@users.noreply.github.com>
2025-02-20 04:15:27 +0800

    Use native promise verion of the function

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 import ChildProcess from "node:child_process";
 import MarkdownItAnchor from "markdown-it-anchor";
-import { promisify } from 'node:util';
 import * as Fs from 'node:fs';
+import * as FsPromises from 'node:fs/promises';
+import * as Util from 'node:util';
 
 export default (eleventyConfig) => {
   const sourceDir = "src";
@@ -33,7 +34,7 @@ export default (eleventyConfig) => {
   // Run after the build ends
   eleventyConfig.on("eleventy.after", async ({ dir }) => {
     const run = (cmd) => {
-      const exec = promisify(ChildProcess.exec);
+      const exec = Util.promisify(ChildProcess.exec);
       return exec(cmd).then(
         (result) => { console.log(result.stdout); },
         (result) => { console.error(result.stderr); }
@@ -51,13 +52,11 @@ export default (eleventyConfig) => {
       const targets = [
         [`${sourceDir}/news.md`, `${dir.output}/news.html`]
       ];
-      const utimes = promisify(Fs.utimes);
-
       const promises = targets.map((target) => {
         const src = target[0];
         const dest = target[1];
         const srcStat = Fs.statSync(src);
-        return utimes(dest, srcStat.atime, srcStat.mtime);
+        return FsPromises.utimes(dest, srcStat.atime, srcStat.mtime);
       });
       return Promise.allSettled(promises);
     };
