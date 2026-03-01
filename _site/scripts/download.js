62a110501cc1df0149a68ba319089dd7144286d9
Chocobo1 <Chocobo1@users.noreply.github.com>
2026-03-02 01:48:30 +0800

    Convert JS script to module

--- _site/scripts/download.js
+++ _site/scripts/download.js
@@ -1,17 +1,14 @@
-"use strict";
 document.addEventListener("DOMContentLoaded", ()=>{
     const distroSelect = document.getElementById("distroSelect");
     const showOption = (element)=>{
         const hideOptionDivs = (element)=>{
             for (const option of element.options)document.getElementById(`${option.value}Div`)?.classList.add("invisible");
         };
-        // reset all
         if (element.id === "OSSelect") {
             hideOptionDivs(element);
             distroSelect.value = "emptyDist";
         }
         hideOptionDivs(distroSelect);
-        // show selected
         document.getElementById(`${element.value}Div`)?.classList.remove("invisible");
     };
     const osSelect = document.getElementById("OSSelect");
